06e5fe987c805260ff10ea73c031acde380ed2c5
Steve Eliopoulos <steve.eliopoulos@gmail.com>
2016-05-09 08:42:25 -0700

    Added multiple employee processing and plot twist

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,42 +1,89 @@
-print "What is your name?"
+count = 1
+
+
+
+print "How many employees will be processed today?  "
+employees=gets.chomp
+employees=employees.to_i
+
+while count <= employees
+
+print "What is your name?  "
 name=gets.chomp
-print "How old are you"	
+
+print "How old are you  "	
 age=gets.chomp
-print"What year were you born??"
+
+print"What year were you born?  "
 birth_year=gets.chomp
-current_year=2016
+
 print "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic=gets.chomp
+
 print "Would you like to enroll in the company’s health insurance? (y/n)"
 insurance=gets.chomp
 
-#If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
 
-if age==#{birth_year}-#{current_year} 
-	age=true
+print "Do you have any allergies? List individually and type 'done' when finished: "
+input=gets.chomp
+
+
+if input == "sunshine"
+	puts "Probalby a VAMPIRE"
 else
-	age=false
+	print "Do you have any allergies? List individually and type 'done' when finished: "
+	input=gets.chomp
+until input == "done" || "sunshine"
+end
 end
 
-if garlic="y"
-	garlic=true
+current_year = 2016
+
+true_age = (current_year.to_i-birth_year.to_i)
+
+if age == true_age
+	age = true
 else
-	garlic=false
+	age = false
 end
 
-if insurance="y"
-	insurance=true
+if garlic == "y"
+	garlic = true
 else
-	insurance=false
+	garlic = false
 end
 
-	puts "Probably not a vampire."
+if insurance == "y"
+	insurance = true
+else
+	insurance = false
+end
 
+if name == "Drake Cula" || "Tu Fang"
+	name = false
+else
+	name = true
+end
 
 #If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
-age!=#{birth_year}-#{current_year} && garlic=false || insurance=false
-	puts do "Probably a vampire"
+
+
+#If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
+if age && (garlic || insurance)
+	puts "Probably not a vampire"
+#If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
+elsif !age && !(garlic || insurance)
+	puts "Probably a vampire"
+#If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+elsif !(age && garlic && insurance)
+	puts "ALmost certainly a vampire"
+#Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
+elsif 
+age && (garlic && insurance)
+else
+	puts "Results inconclusive"
+end
+count +=1
 end
 
-
-
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
