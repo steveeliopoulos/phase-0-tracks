28b3bac67039513049de0bf8952c1a44c487154b
Steve Eliopoulos <steve.eliopoulos@gmail.com>
2016-05-17 17:13:07 -0700

    Comment puppy species/behavior characteristics.

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------------
+# happy_pup
+
+# CHARACTERISTICS ----------------
+# Tail length: varies
+# Friendly: always
+# Spot count: varies
+# Name: varies
+# Color: white w/ black spots
+
+# BEHAVIOR -----------------------
+# Run
+# Lick
+# Play
