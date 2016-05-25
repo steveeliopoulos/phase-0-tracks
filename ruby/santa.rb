6dc316c3b305b801f04454eb1da7d646b96cf3b4
Steve Eliopoulos <steve.eliopoulos@gmail.com>
2016-05-24 18:06:24 -0700

    Reworked release 4 with Hannah in office hours

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,18 +2,15 @@ class Santa
 	attr_accessor :age, :gender, :ethnicity, :reindeer_ranking
 	attr_reader :name
 
-		def initialize(name, gender, ethnicity, reindeer_ranking)
+	def initialize(name, gender, ethnicity)
 		@name = name
 		puts "Initialize Santa instance for #{@name}..."
 		@gender = gender
 		@ethnicity = ethnicity
-		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		
+		@age = 0
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]		
 	end
-	def age
-		age = 0
-		@age 
-	end
+	
 	def speak
 		puts "#{@name} give us your best, 'Ho, ho, ho! Happppy holidays!' "
 	end
@@ -22,10 +19,11 @@ class Santa
 		puts "Input cookie type: "
 		cookie=gets.chomp
 		puts "That was a good #{cookie}!"
-	
+	end
+
 	def celebrate_birthday
-		age +1
-		puts age
+		@age+=1
+		puts @age
 	end
 	
 	
@@ -46,39 +44,45 @@ class Santa
 	end
 	
 end
-end
+
+# new_var = Santa.new("Mr. C", "male", "North-Polean")
+# new_var.speak
+# new_var.eat_milk_and_cookies
+# new_var.celebrate_birthday
 
 
-
-class Reindeer
-  def initialize(name)
-    @name = name
-  end
+# class Reindeer
+#   def initialize(name)
+#     @name = name
+#   end
 santas = []
 
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-reindeer = Reindeer.new("Blitzen")
-
-
-
-puts "Iterating through 'speak' method"
-santas.each do |merry_christmas|
-	puts merry_christmas.speak
+# example_genders.length.times do |i|
+# #reindeer = Reindeer.new("Blitzen")
+10.times do 
+	santas << Santa.new("Mrs. Claus", example_genders.sample, "white")
 end
+puts santas
+
+# puts "Iterating through 'speak' method"
+# santas.each do |merry_christmas|
+# 	puts merry_christmas.speak
+# end
 
 
 
-santas << Santa.new("Rudolph","agender", "black", 1)
-santas << Santa.new("female", "Latino", '', '')
-santas << Santa.new("bigender", "white" ,'', '')
-santas << Santa.new("male", "Japanese",'', '')
-santas << Santa.new("female", "prefer not to say", '', '')
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)",'', '')
-santas << Santa.new("N/A", "N/A", '', '')
-santas << Santa.new(example_genders[i], example_ethnicities[i], '','')
+# santas << Santa.new("Rudolph","agender", "black", 1)
+# santas << Santa.new("female", "Latino", '', '')
+# santas << Santa.new("bigender", "white" ,'', '')
+# santas << Santa.new("male", "Japanese",'', '')
+# santas << Santa.new("female", "prefer not to say", '', '')
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)",'', '')
+# santas << Santa.new("N/A", "N/A", '', '')
+# santas << Santa.new(example_genders[i], example_ethnicities[i], '','')
 
 
-end
-end
+
+
+
