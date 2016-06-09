827780fade8d8c8764309dd2f51373a000671197
Steve Eliopoulos <steve.eliopoulos@gmail.com>
2016-06-09 08:36:21 -0700

    Update 6.1 with comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+SPECIES ------------------------
+Labrador
+
+CHARACTERISTICS ----------------
+Height: varies
+Length: varies
+Friendly: yes
+Eye color: varies
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Run
+Walk
+Sleep
+Eat
