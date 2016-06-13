8285761908d862f2bd89e4f161d7c1c6fc888c02
Steve Eliopoulos <steve.eliopoulos@gmail.com>
2016-06-13 07:05:39 -0700

    Refactor classes with mixin for 6.5

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,37 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yelling_happily(happy_words)
-  	happy_words + '!!'
-  end
+#   def self.yelling_happily(happy_words)
+#   	happy_words + '!!'
+#   end
 
+# end
+
+# p Shout.yelling_happily("Yay, I'm yelling happily for some reason")
+# p Shout.yell_angrily("Boooo! Boo on you, dude")
+
+
+module ShoutMixin
+	def loud_noises(string)
+		puts string + '!!!!!'
+	end
 end
 
-p Shout.yelling_happily("Yay, I'm yelling happily for some reason")
-p Shout.yell_angrily("Boooo! Boo on you, dude")
+class Fan
+	include ShoutMixin
+end
+
+class Ump
+	include ShoutMixin
+end
+
+unruly = Fan.new
+unruly.loud_noises('you suck blue')
+
+overweight_alcoholic = Ump.new
+overweight_alcoholic.loud_noises('you are outta here')
+
+
+
